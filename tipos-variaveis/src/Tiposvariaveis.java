bc8b9b058afa2110b38c23a74d270f60fa5614e0
rebecamb <rebecambdev@gmail.com>
2023-01-30 21:21:47 -0300

    adiciona exemplos do uso de tipos e variáveis

---
+++ tipos-variaveis/src/Tiposvariaveis.java
@@ -0,0 +1,10 @@
+public class Tiposvariaveis {
+    public static void main(String[] args) throws Exception {
+       int gastoTotal;
+       int gastoJaneiro = 35000;
+       int gastoFevereiro = 78000;
+       int gastoMarco = 19000;
+       gastoTotal = gastoJaneiro + gastoFevereiro + gastoMarco;
+       System.out.println("O gasto trimestral da empresa é  " + gastoTotal);
+}
+}
